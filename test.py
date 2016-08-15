6ef9e888e0caaa8d07c350b54bea5b8ea0c15eeb
Jon Rao <jonrao@mathworks.com>
2016-08-15 16:32:19 -0400

    Initial commit

---
+++ test.py
@@ -0,0 +1,29 @@
+# Try to learn the methods in pandas
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+
+# Generate date times
+
+# First way to generate it
+dt = datetime(2016, 8, 15)
+end = datetime(2016, 8, 25)
+step = timedelta(days=1)
+
+result = []
+
+while dt < end:
+    result.append(dt.strftime('%Y-%m-%d'))
+    dt += step
+
+# Second way with Pandas
+result_pd = pd.date_range('2016-8-15', periods = 5)
+
+# Generate timeseries
+ts = pd.Series(np.random.randn(5), index = result_pd)
+
+
+# Method Collection
+
+# Slicing: truncate
+ts.truncate(after = '2016-08-18')
